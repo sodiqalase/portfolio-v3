7e980ac494cc8fc3afaa5ddfd3a4103d09175421
Olamide Sholanke <olamidesholanke@Olamides-MacBook-Pro.local>
2021-06-11 08:25:58 +0100

    disabled locomotive scroll on mobile

--- layouts/default.vue
+++ layouts/default.vue
@@ -22,7 +22,6 @@ export default {
       window.locomotiveScroll = new LocomotiveScroll({
         el: locomote,
         smooth: true,
-        smartphone: { smooth: true },
       });
     })
   }
