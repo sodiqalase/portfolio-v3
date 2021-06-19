749d23aaae100cfc9729bdb12f3c62902fb56b21
Olamide Sholanke <olamidesholanke@Olamides-MacBook-Pro.local>
2021-06-19 14:38:23 +0100

    upgrade: meta data

--- nuxt.config.js
+++ nuxt.config.js
@@ -14,7 +14,7 @@ export default {
     meta: [
       { charset: "utf-8" },
       { name: "viewport", content: "width=device-width, initial-scale=1" },
-      { hid: "description", name: "description", content: "" }
+      { hid: "description", name: "description", content: "Hey there, I am Olamide, and I'm a Frontend Engineer currently based in Lagos Nigeria." }
     ],
     link: [{ rel: "icon", type: "image/x-icon", href: "/favicon.ico" }]
   },
